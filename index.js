6df8363982e2be7c41d3800a160ec3008455de05
davidpablo007 <davidpablo007@icloud.com>
2023-08-26 21:12:18 +0100

    DS

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const myString= "Hello, friend";
+
+/*
+DATA STRUCTURE
+1. To find the sum of distinct elements from set1 and set2, we can combine and remove
+duplicates. The distinct elements are [3,1,7,9,2,4].The sum of these distinct elements is
+*26 */
