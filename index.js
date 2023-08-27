2a333d749dff6d6d1a3d79b34f5b931a4bff568b
davidpablo007 <davidpablo007@icloud.com>
2023-08-27 11:31:52 +0100

    Array

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ DATA STRUCTURE
 1. To find the sum of distinct elements from set1 and set2, we can combine and remove
 duplicates. The distinct elements are [3,1,7,9,2,4].The sum of these distinct elements is
 *26 */
+
+const myArray = [1, 2, 3, "hello", true];
